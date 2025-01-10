b3874b6f47779f6a10a8c8281088e09514714709
Cosay09 <childrengang8@gmail.com>
2025-01-10 22:46:56 +0600

    ok

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    int t;
+    for (cin >> t; t-->0)
+    {
+        int n, k, a;
+        map<int, int> A;
+        for (cin >> n >> k; n --> 0)
+            cin >> a; A[a]++;
+        
+    }
+}
