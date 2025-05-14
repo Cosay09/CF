1e25ccfcf28d804d4d7b76a8888176b8d9c48c2e
Saydul Islam <childrengang8@gmail.com>
2025-05-14 17:54:28 +0600

    Update sayed.c

--- sayed.c
+++ sayed.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
     struct Node* left;
     struct Node* right;
 };
 
-struct Node* createNode(int data) {
+struct Node* createNode(int data)
+{
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->left = NULL;
@@ -15,34 +17,63 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) {
+struct Node* insert(struct Node* root, int data)
+{
+    if (root == NULL)
+    {
         return createNode(data);
     }
 
-    if (data < root->data) {
+    if (data < root->data)
+    {
         root->left = insert(root->left, data);
-    } else {
+    }
+    else
+    {
         root->right = insert(root->right, data);
     }
 
     return root;
 }
 
-void printInOrder(struct Node* root) {
-    if (root != NULL) {
+void printInOrder(struct Node* root)
+{
+    if (root != NULL)
+    {
         printInOrder(root->left);
         printf("%d ", root->data);
         printInOrder(root->right);
     }
 }
 
-int main() {
+void printPreOrder(struct Node* root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        printPreOrder(root->left);
+        printPreOrder(root->right);
+    }
+}
+
+void printPostOrder(struct Node* root)
+{
+    if (root != NULL)
+    {
+        printPostOrder(root->left);
+        printPostOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
     struct Node* root = NULL;
 
     int elements[10] = {15, 10, 25, 20, 5, 30, 35, 40, 3, 8};
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         root = insert(root, elements[i]);
     }
 
@@ -50,4 +81,13 @@ int main() {
     printInOrder(root);
     printf("\n");
 
+    printf("Pre-order traversal: ");
+    printPreOrder(root);
+    printf("\n");
+
+    printf("Post-order traversal: ");
+    printPostOrder(root);
+    printf("\n");
+
+    return 0;
 }
