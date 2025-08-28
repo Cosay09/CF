6a7df938cdc43d507eee75a345845ad36de770b9
Saydul Islam <childrengang8@gmail.com>
2025-08-28 14:39:20 +0600

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -3,10 +3,12 @@
 
 using namespace std;
 
-int partition(int arr[], int left, int right) {
+int partition(int arr[], int left, int right) 
+{
     int pivot = arr[right];
     int i = left - 1;
-    for (int j = left; j < right; ++j) {
+    for (int j = left; j < right; ++j) 
+    {
         if (arr[j] < pivot) {
             ++i;
             swap(arr[i], arr[j]);
@@ -16,20 +18,27 @@ int partition(int arr[], int left, int right) {
     return i + 1;
 }
 
-void quicksort(int arr[], int left, int right) {
-    if (left < right) {
+void quicksort(int arr[], int left, int right) 
+{
+    if (left < right) 
+    {
         int pi = partition(arr, left, right);
         quicksort(arr, left, pi - 1);
         quicksort(arr, pi + 1, right);
     }
 }
 
-int main() {
+int main() 
+{
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
+    
     quicksort(arr, 0, n - 1);
-    for (int i = 0; i < n; ++i) {
+    
+    for (int i = 0; i < n; ++i) 
+    {
         cout << arr[i] << " ";
     }
+    
     cout << endl;
 }
