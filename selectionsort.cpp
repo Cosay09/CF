10c3c6ad0df9f9f27b54ab7180390470517bd3c0
Saydul Islam <childrengang8@gmail.com>
2025-08-28 14:38:48 +0600

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++) 
+    {
         int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (arr[j] < arr[min_idx]) 
+            {
                 min_idx = j;
             }
         }
