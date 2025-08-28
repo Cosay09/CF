494f001af7216f1abe868750d48726e64588f4e0
Saydul Islam <childrengang8@gmail.com>
2025-08-28 14:42:38 +0600

    Update greedyactivityselector.cpp

--- greedyactivityselector.cpp
+++ greedyactivityselector.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int start[]  = {1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12};
     int finish[] = {4, 5, 6, 7, 9, 9, 10,11,12,14,16};
     int n = sizeof(start) / sizeof(start[0]);
@@ -13,15 +14,18 @@ int main() {
     selectedIndices[count++] = 0;
     int k = 0;
 
-    for (int m = 1; m < n; m++) {
-        if (start[m] >= finish[k]) {
+    for (int m = 1; m < n; m++)
+        {
+        if (start[m] >= finish[k]) 
+        {
             selectedIndices[count++] = m;
             k = m;
         }
     }
 
     cout << "Selected activities: ";
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) 
+    {
         int idx = selectedIndices[i];
         cout << "(" << start[idx] << "," << finish[idx] << ") ";
     }
